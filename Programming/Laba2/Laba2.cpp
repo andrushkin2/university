573996a456060541b46cc007f0d0469a0d7412e7
John Doekosiak_man <andrushkin2@live.ru>
2015-03-16 23:13:17 +0300

    Created laba2 folder

---
+++ Programming/Laba2/Laba2.cpp
@@ -0,0 +1,9 @@
+//
+//  Laba2.cpp
+//  
+//
+//  Created by Andrei Kozyakov on 16.03.15.
+//
+//
+
+#include "Laba2.h"
---
+++ Programming/Laba2/Laba2.h
@@ -0,0 +1,14 @@
+//
+//  Laba2.h
+//  
+//
+//  Created by Andrei Kozyakov on 16.03.15.
+//
+//
+
+#ifndef ____Laba2__
+#define ____Laba2__
+
+#include <stdio.h>
+
+#endif /* defined(____Laba2__) */
