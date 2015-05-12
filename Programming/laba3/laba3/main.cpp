437b1007418efa653797fc8474fcae85296595dc
kosiak_man <andrushkin2@live.ru>
2015-05-11 23:25:50 -0400

    Added laba with string

---
+++ Programming/laba3/laba3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  laba3
+//
+//  Created by Andrei Kozyakov on 11.05.15.
+//  Copyright (c) 2015 Andrei Kozyakov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Programming/laba3/laba3/untitled folder/stringClass.cpp
@@ -0,0 +1,9 @@
+//
+//  stringClass.cpp
+//  laba3
+//
+//  Created by Andrei Kozyakov on 11.05.15.
+//  Copyright (c) 2015 Andrei Kozyakov. All rights reserved.
+//
+
+#include "stringClass.h"
