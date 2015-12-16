91cc018b63abb355c36a368aace534f0e6e3485c
kosiak_man <andrushkin2@live.ru>
2015-12-16 07:09:08 -0500

    Updated third lab of asm

--- Programming/laba3.asm
+++ Programming/laba3.asm
@@ -2,14 +2,14 @@
  include emu8086.inc   
 
 data segment  
-    HANDLE dw 0 
-    HANDLE1 dw 0       
-    count dw 0    
-    PATH    db   200, 199 dup('$') ;'C:\emuread.txt',0
-    PATH_RES    db    'C:\result.txt',0   
-    buf     db 200, 199 dup('$')    
-    key     db 200, 199 dup('$')
-    pkey db "press any key...$"
+    readHandle      dw 0 
+    writeHandle     dw 0       
+    count           dw 0    
+    filePath        db 200, 199 dup('$') ;'C:\file2.txt',0
+    resultPath      db 'C:\result.txt',0   
+    buf             db 200, 199 dup('$')    
+    key             db 200, 199 dup('$')
+    pkey            db "press any key...$"
 ends
 
 stack segment
@@ -23,10 +23,10 @@ start:
     mov ds, ax
     mov es, ax
 
-    ;enter a path to file with data
-    PRINT 'Enter PATH:'
-    lea di,PATH
-    mov dx,200
+    ;enter a filePath to file with data
+    PRINT 'Enter path to a file:'
+    lea di, filePath
+    mov dx, 200
     CALL GET_STRING  
 
     ;enter a key for searching
@@ -44,42 +44,42 @@ start:
     mov [si], '$'    ; replace last symbol to $ for correct print by ah=9 dos function 
     
     ;open file for writing    
-    mov ah,3Ch              
-    lea dx,PATH_RES       
-    xor cx,cx               
+    mov ah, 3Ch              
+    lea dx, resultPath       
+    xor cx, cx               
     int 21h                 
     jc onError               
-    mov [HANDLE1],ax 
+    mov [writeHandle], ax 
 
     ;open file for reading 
     mov ah, 3Dh
-    mov al,2         
-    lea dx,PATH      
+    mov al, 2         
+    lea dx, filePath      
     int 21h
     jc onError    
-    mov HANDLE, ax   
+    mov readHandle, ax   
     
-    mov bx,ax       
-    xor cx,cx
-    xor dx,dx 
-    xor di,di
-    mov ax,4200h
+    mov bx, ax       
+    xor cx, cx
+    xor dx, dx 
+    xor di, di
+    mov ax, 4200h
     int 21h 
 ; mark for reading next one symbol from file
 readNextSymbol:   
     mov bx, count
-    mov ah,3fh      
-    mov cx,1
-    xor dx,dx       
-    lea dx,buf[bx]
-    mov bx,HANDLE
+    mov ah, 3fh      
+    mov cx, 1
+    xor dx, dx       
+    lea dx, buf[bx]
+    mov bx, readHandle
     int 21h
     
     mov bx, count           
-    cmp buf[bx],0ah
+    cmp buf[bx], 0ah
     je searchKeyInRow
     ;if end of file -> exit    
-    cmp ax,cx       
+    cmp ax, cx       
     jnz exit
     ;increment counter and repeat reading a new symbol
     inc count  
@@ -119,30 +119,30 @@ writeToFile:
 
     ;open result file for writing
     mov ah, 3Dh
-    mov al,2         
-    lea dx,PATH_RES      
+    mov al, 2         
+    lea dx, resultPath      
     int 21h
     jc  onError        
-    mov HANDLE1, ax         
-    mov bx,ax   
+    mov writeHandle, ax         
+    mov bx, ax   
     mov ah, 42h
-    mov al,2        
-    mov dx,0
-    xor cx,cx 
-    mov bx,HANDLE1
+    mov al, 2        
+    mov dx, 0
+    xor cx, cx 
+    mov bx, writeHandle
     int 21h 
 
     ;write to file row 
-    mov ah,40h              
-    lea dx,buf           
-    mov cx,[count]         
+    mov ah, 40h              
+    lea dx, buf           
+    mov cx, [count]         
     int 21h
     jc onError     
               
 ;close file opened for writing 
 closeFile:
-    mov ah,3Eh              
-    mov bx,[HANDLE1]         
+    mov ah, 3Eh              
+    mov bx, [writeHandle]         
     int 21h                 
     jnc clearRow                
 
@@ -150,24 +150,24 @@ closeFile:
 clearRow:     
     cld
     lea di, buf 
-    mov al,'$'
+    mov al, '$'
     mov cx, 200
     rep stosb 
 
-    xor bx,bx 
-    xor cx,cx
+    xor bx, bx 
+    xor cx, cx
     mov ah, 42h
-    mov al,1
-    mov dx,10  
+    mov al, 1
+    mov dx, 10  
     int 21h
   
     ;reset count to 0 and jump to read a new line in file
-    mov count,0
+    mov count, 0
     jmp readNextSymbol
  
 ; mark of exit program   
 exit:            
-    mov ah,4ch
+    mov ah, 4ch
     int 21h    
     ;prinit waiting message            
     lea dx, pkey
