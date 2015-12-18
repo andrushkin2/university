a370f4a38551d271d90111bdb08672abcb098b29
kosiak_man <andrushkin2@live.ru>
2015-12-18 16:15:33 +0300

    Updated forth lab of asm

--- Programming/laba4.asm
+++ Programming/laba4.asm
@@ -4,19 +4,51 @@ data segment
     readHandle      dw 0
     count           dw 0    
     filePath        db 200, 199 dup('$') ;'C:\file2.txt',0
-    buf             db 400, 399 dup('$')
+    buf             db 200, 199 dup('$')
     pkey            db "press any key...$"
+    old_cs dw 0
+    old_ip dw 0 
 ends
 
 stack segment
     dw   128  dup(0)
 ends
-;======================================================================
+;======================================================================    
+code segment
+
+NEW_INT PROC FAR
+    push ax;сохраняем все изменяемые регистры
+    push dx
+    ; wait for any key....    
+    mov ah, 1
+    int 21h
+
+    pop dx ;восстанавливаем регистры
+    pop ax
+    iret
+NEW_INT ENDP 
+
+
 start:
 ; set segment registers:
     mov ax, data
     mov ds, ax
     mov es, ax
+
+    mov ah, 35h ; функция получения вектора
+    mov al, 23h ; номер вектора
+    int 21h
+    mov old_ip, bx ; запоминание смещения
+    mov old_cs, es ; и сегмента
+
+    push ds
+    mov dx, offset NEW_INT ; смещение для процедуры в DX
+    mov ax, seg NEW_INT ; сегмент процедуры
+    mov ds, ax ; помещаем в DS
+    mov ah, 25h ; функция установки вектора
+    mov al, 23h ; номер вектора
+    int 21h ; меняем прерывание
+    pop ds
 
     ;enter a filePath to file with data
     PRINT 'Enter path to a file:'
@@ -50,14 +82,14 @@ readNextSymbol:
     mov bx, readHandle
     int 21h
     
-    mov bx, count 
-    ;print symbol to console
-    lea dx, buf[bx]
-    mov ah, 9
-    int 21h            
+    mov bx, count          
     ;if end of file -> exit    
     cmp ax, cx       
     jnz exit
+    ;print symbol to console
+    lea dx, buf[bx]
+    mov ah, 9
+    int 21h   
     ;increment counter and repeat reading a new symbol
     inc count  
     jmp readNextSymbol     
@@ -68,7 +100,18 @@ onError:
     
 ;======================================================================     
 ; mart for exit program
-exit:              
+exit: 
+    cli
+    push ds ;DS будет разрушен
+    mov dx, old_ip ;подготовка к восстановлению
+    mov ax, old_cs ;
+    mov ds, ax ;подготовка к восстановлению
+    mov ah, 25h ;функция установки вектора
+    mov al, 23h ;номер вектора
+    int 21h ;восстанавливаем вектор
+    pop ds ;восстанавливаем DS
+    sti
+
     ;prinit waiting message            
     lea dx, pkey
     mov ah, 9
