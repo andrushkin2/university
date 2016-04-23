6dda1677d90c01d146e2b03cde5aac496d5cbdd0
kosiak_man <andrushkin2@live.ru>
2016-04-23 15:57:41 +0300

    Updated labs

--- apk/soundTimer.cpp
+++ apk/soundTimer.cpp
@@ -1,13 +1,17 @@
 // soundTimer.cpp : Defines the entry point for the console application.
 //
 
+#include <iostream>
 #include <sys/io.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <termios.h>
+#include <unistd.h>
+
+using namespace std;
 
 void runDelayInTicks(int);
 void playSound(int, int);
+int enablePermissions(bool);
 int main();
 
 termios stored;
@@ -22,55 +26,68 @@ void getch_init() {
     settings.c_cc[VMIN] = 1;        //Размер буфера для ожиания - 1 символ
     tcsetattr(STDIN_FILENO, TCSANOW, &settings);    //Применяем новые настройки
 }
-
 void getch_fin() {
     tcsetattr(STDIN_FILENO, TCSANOW, &stored);      //Восстанавливаем настройки терминала
 }
 
 int main()
 {
-	if(ioperm(0x42, 2, 1)) {
-        perror("Error getting access to timer ports");
-        return 1;
-    }
-	if(ioperm(0x61, 2, 1)) {
-        perror("Error getting access to timer ports");
-        return 1;
-    }
-	getch_init();
-	playSound(330, 2);
-	getchar();
-	getch_fin();
+	if (enablePermissions(true)) {
+		return 1;
+	}
 	
-	if(ioperm(0x42, 2, 0)) {
-        perror("Error getting access to timer ports");
-        return 1;
-    }
-	if(ioperm(0x61, 2, 0)) {
-        perror("Error getting access to timer ports");
-        return 1;
-    }
+	//getch_init();
+	playSound(330, 5);
+	//getchar();
+	//getch_fin();
 	
+	if (enablePermissions(false)) {
+		return 1;
+	}
+
     return 0;
 }
 
 void runDelayInTicks(int steps) {
-	sleep(steps);
+	usleep(steps * 1000000);
 }
 
 void playSound(int freq, int delayTime) {
 	int delay, i;
+	unsigned char _0x0061_port;
 	//	set mode chanel of 2 timer
-	outb_p(0x43, 0xb6);
+	outb_p(0xb6, 0x0043);
 	//	calc delay for the timer's counter register
 	delay = 1193180L / freq;
 	//	set to the timer register young byte and then older byte 
-	outb_p(0x42, delay & 0x00ff);
-	outb_p(0x42, (delay & 0xff00) >> 8);
+	outb_p(delay & 0x00ff, 0x0042);
+	outb_p((delay & 0xff00) >> 8, 0x0042);
 	//	turn on speaker
-	outb_p(0x61, inb_p(0x61) | 3);
+	_0x0061_port = inb_p(0x0061);
+    _0x0061_port |= 0x03;
+    outb_p(_0x0061_port, 0x0061);
 	//	start delay
 	runDelayInTicks(delayTime);
 	//	turn off speaker
-	outb_p(0x61, inb_p(0x61) & 0xfc);
+	_0x0061_port = inb_p(0x0061);
+    _0x0061_port &= 0xFC;
+    outb_p(_0x0061_port, 0x0061);
 }   
+
+int enablePermissions(bool enable)
+{
+	int value = enable ? 1 : 0;
+	if(ioperm(0x0042, 2, value)) {
+        perror("Error getting access to timer ports");
+        return 1;
+    }
+	if(ioperm(0x0061, 1, value)) {
+        perror("Error getting access to timer ports");
+        return 1;
+    }
+	if(ioperm(0x0080, 1, value)) {
+        perror("Error getting access to 80 port");
+        return 1;
+    }
+	return 0;
+}
