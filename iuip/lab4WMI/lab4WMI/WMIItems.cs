a1a2e66aebf0b843502e7e4a8a261fc5e2713e57
kosiak_man <andrushkin2@live.ru>
2016-05-13 09:22:18 +0300

    Added 4s lab of iipy

---
+++ iuip/lab4WMI/lab4WMI/WMIItems.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4WMI
+{
+    public static class WMIEntityName
+    {
+        public static string PlugAndPlay = "Win32_PnPEntity";
+        public static string SystemEnclosure = "Win32_SystemEnclosure";
+        public static string BaseBoard = "Win32_BaseBoard";
+        public static string MotherboardDevice = "Win32_MotherboardDevice";
+        public static string MotherboardOnBoardDevice = "Win32_OnBoardDevice";
+        public static string MotherboardBus = "Win32_Bus";
+        public static string SystemSlot = "Win32_SystemSlot";
+        public static string Processor = "Win32_Processor";
+        public static string CacheMemory = "Win32_CacheMemory";
+        public static string PhysicalMemory = "Win32_PhysicalMemory";
+        public static string MemoryDevice = "Win32_MemoryDevice";
+        public static string MemoryArray = "Win32_MemoryArray";
+        public static string PhysicalMemoryArray = "Win32_PhysicalMemoryArray";
+        public static string DeviceMemoryAddress = "Win32_DeviceMemoryAddress";
+        public static string DMAChannel = "Win32_DMAChannel";
+        public static string PortConnector = "Win32_PortConnector";
+        public static string PortResource = "Win32_PortResource";
+        public static string ParallelPort = "Win32_ParallelPort";
+        public static string SerialPort = "Win32_SerialPort";
+        public static string Keyboard = "Win32_Keyboard";
+        public static string PointingDevice = "Win32_PointingDevice";
+        public static string SoundDevice = "Win32_SoundDevice";
+        public static string VideoController = "Win32_VideoController";
+        public static string CIMVideoControllerResolution = "CIM_VideoControllerResolution";
+        public static string NetworkAdapter = "Win32_NetworkAdapter";
+        public static string NetworkAdapterConfiguration = "Win32_NetworkAdapterConfiguration";
+        public static string DesktopMonitor = "Win32_DesktopMonitor";
+        public static string FloppyDrive = "Win32_FloppyDrive";
+        public static string DiskDrive = "Win32_DiskDrive";
+        public static string CDROMDrive = "Win32_CDROMDrive";
+    }
+}
