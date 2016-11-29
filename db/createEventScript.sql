b01ec4d5d815752f52c4cd64f3ce5de3c264eea4
kosiak_man <andrushkin2@live.ru>
2016-11-29 09:57:45 -0500

    Updated eventScript

--- db/createEventScript.sql
+++ db/createEventScript.sql
@@ -1,8 +1,55 @@
 DELETE FROM event;
 
+select @clientId:= 111111,  @holeId:= 1, @serviceId:= 1, @emplyeeId:= 111115, @date:= '2016-11-29 14:00:00';
+
 INSERT into event (date, holeId, clientId, serviceId, emplyeeId ) 
-	SELECT distinct '2016-11-29 14:00:00' as date, 1 as holeId, 111111 as clientId, 1 as serviceId, 111115 as emplyeeId
+	SELECT distinct @date as date, @holeId as holeId, @clientId as clientId, @serviceId as serviceId, @emplyeeId as emplyeeId
 		FROM serviceemployeehash 
-			WHERE exists(select * from serviceemployeehash where serviceId = 1 and employeeId = 111115)
-				AND exists(select * from hole where number = 1)
-                AND exists(select * from client where passportId = 111111);
+			WHERE exists(select * from serviceemployeehash where serviceId = @serviceId and employeeId = @emplyeeId)
+				AND exists(select * from hole where number = @holeId)
+                AND exists(select * from client where passportId = @clientId);
+                
+select @clientId:= 111111,  @holeId:= 1, @serviceId:= 4, @emplyeeId:= 111117, @date:= '2016-11-29 15:00:00';
+
+INSERT into event (date, holeId, clientId, serviceId, emplyeeId ) 
+	SELECT distinct @date as date, @holeId as holeId, @clientId as clientId, @serviceId as serviceId, @emplyeeId as emplyeeId
+		FROM serviceemployeehash 
+			WHERE exists(select * from serviceemployeehash where serviceId = @serviceId and employeeId = @emplyeeId)
+				AND exists(select * from hole where number = @holeId)
+                AND exists(select * from client where passportId = @clientId);
+
+select @clientId:= 111111,  @holeId:= 1, @serviceId:= 4, @emplyeeId:= 111117, @date:= '2016-11-28 10:00:00';
+
+INSERT into event (date, holeId, clientId, serviceId, emplyeeId ) 
+	SELECT distinct @date as date, @holeId as holeId, @clientId as clientId, @serviceId as serviceId, @emplyeeId as emplyeeId
+		FROM serviceemployeehash 
+			WHERE exists(select * from serviceemployeehash where serviceId = @serviceId and employeeId = @emplyeeId)
+				AND exists(select * from hole where number = @holeId)
+                AND exists(select * from client where passportId = @clientId);
+
+select @clientId:= 111111,  @holeId:= 5, @serviceId:= 4, @emplyeeId:= 111117, @date:= '2016-11-28 15:00:00';
+
+INSERT into event (date, holeId, clientId, serviceId, emplyeeId ) 
+	SELECT distinct @date as date, @holeId as holeId, @clientId as clientId, @serviceId as serviceId, @emplyeeId as emplyeeId
+		FROM serviceemployeehash 
+			WHERE exists(select * from serviceemployeehash where serviceId = @serviceId and employeeId = @emplyeeId)
+				AND exists(select * from hole where number = @holeId)
+                AND exists(select * from client where passportId = @clientId);
+                
+select @clientId:= 111111,  @holeId:= 6, @serviceId:= 2, @emplyeeId:= 111117, @date:= '2016-11-27 10:00:00';
+
+INSERT into event (date, holeId, clientId, serviceId, emplyeeId ) 
+	SELECT distinct @date as date, @holeId as holeId, @clientId as clientId, @serviceId as serviceId, @emplyeeId as emplyeeId
+		FROM serviceemployeehash 
+			WHERE exists(select * from serviceemployeehash where serviceId = @serviceId and employeeId = @emplyeeId)
+				AND exists(select * from hole where number = @holeId)
+                AND exists(select * from client where passportId = @clientId);
+
+select @clientId:= 111111,  @holeId:= 5, @serviceId:= 4, @emplyeeId:= 111117, @date:= '2016-11-27 11:00:00';
+
+INSERT into event (date, holeId, clientId, serviceId, emplyeeId ) 
+	SELECT distinct @date as date, @holeId as holeId, @clientId as clientId, @serviceId as serviceId, @emplyeeId as emplyeeId
+		FROM serviceemployeehash 
+			WHERE exists(select * from serviceemployeehash where serviceId = @serviceId and employeeId = @emplyeeId)
+				AND exists(select * from hole where number = @holeId)
+                AND exists(select * from client where passportId = @clientId);
