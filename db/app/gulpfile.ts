63b15b2cbd6d67837a7a95cb7cd6579637b33dad
kosiak_man <andrushkin2@live.ru>
2016-12-07 09:56:46 -0500

    Added a db app

---
+++ db/app/gulpfile.ts
@@ -0,0 +1,31 @@
+
+import gulp = require("gulp");
+import fs = require("fs");
+import ts = require("gulp-typescript");
+
+let serve = require("gulp-serve"),
+    devFolderPath = "./dev",
+    outPut = "./bin/",
+    outPutTs = `${outPut}ts/`,
+    outPutJs = `${outPut}js/`;
+
+gulp.task("tsBuild", function() {
+    let tsProject: ts.Project = ts.createProject("./tsconfig.json"),
+        typescriptResult = gulp.src(`${devFolderPath}/**/*.ts`).pipe(<any>ts(tsProject));
+    return typescriptResult.js.pipe(gulp.dest("./build"));
+});
+
+gulp.task("serve", serve({
+    root: ["public", "build"],
+    port: 3000,
+    https: true,
+    middleware: function(req, res) {}    // custom optional middleware
+}));
+
+gulp.task("default", () => {
+
+});
+
+gulp.task("watcher", function() {
+    //gulp.watch();
+});
---
+++ db/app/typings/tsd.d.ts
@@ -0,0 +1,7 @@
+
+/// <reference path="webix/webix.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="gulp/gulp.d.ts" />
+/// <reference path="orchestrator/orchestrator.d.ts" />
+/// <reference path="q/Q.d.ts" />
+/// <reference path="vinyl/vinyl.d.ts" />
