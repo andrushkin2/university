18255e500872f0bf09199eecaba3bcbbbc2386c2
kosiak_man <andrushkin2@live.ru>
2017-04-08 15:43:00 +0300

    Update py-scripts

--- cosi/lab1/bundle.py
+++ cosi/lab1/bundle.py
@@ -5,7 +5,7 @@ import subprocess as process
 def bundle():
     """Fucntion run Browserify compiller for all JS-files > bundle.js
     """
-    exec_process = process.Popen(["browserify", ".", ">", "bundle.js"], shell=True,
+    exec_process = process.Popen(["browserify", "./", ">", "bundle.js"], shell=True,
                                  stdout=process.PIPE)
     for line in exec_process.stdout:
         print(line)
