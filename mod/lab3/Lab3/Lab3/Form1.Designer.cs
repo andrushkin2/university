8218220225a92be7145a2e53da4d751ad4823438
kosiak_man <andrushkin2@live.ru>
2017-12-28 00:00:22 +0300

    Add lab3 mod

---
+++ mod/lab3/Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.p1 = new System.Windows.Forms.TextBox();
+            this.p2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.n = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.run = new System.Windows.Forms.Button();
+            this.a = new System.Windows.Forms.TextBox();
+            this.aeqwe = new System.Windows.Forms.Label();
+            this.l = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.w = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "P1:";
+            // 
+            // p1
+            // 
+            this.p1.Location = new System.Drawing.Point(111, 47);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(88, 31);
+            this.p1.TabIndex = 1;
+            this.p1.Text = "0.8";
+            // 
+            // p2
+            // 
+            this.p2.Location = new System.Drawing.Point(292, 47);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(88, 31);
+            this.p2.TabIndex = 3;
+            this.p2.Text = "0.5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "P2:";
+            // 
+            // n
+            // 
+            this.n.Location = new System.Drawing.Point(504, 47);
+            this.n.Name = "n";
+            this.n.Size = new System.Drawing.Size(88, 31);
+            this.n.TabIndex = 5;
+            this.n.Text = "100000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(452, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "N:";
+            // 
+            // run
+            // 
+            this.run.Location = new System.Drawing.Point(649, 47);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(139, 51);
+            this.run.TabIndex = 6;
+            this.run.Text = "Run";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // a
+            // 
+            this.a.Location = new System.Drawing.Point(111, 161);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(88, 31);
+            this.a.TabIndex = 8;
+            this.a.Text = "0.8";
+            // 
+            // aeqwe
+            // 
+            this.aeqwe.AutoSize = true;
+            this.aeqwe.Location = new System.Drawing.Point(61, 164);
+            this.aeqwe.Name = "aeqwe";
+            this.aeqwe.Size = new System.Drawing.Size(32, 25);
+            this.aeqwe.TabIndex = 7;
+            this.aeqwe.Text = "A:";
+            // 
+            // l
+            // 
+            this.l.Location = new System.Drawing.Point(111, 219);
+            this.l.Name = "l";
+            this.l.Size = new System.Drawing.Size(88, 31);
+            this.l.TabIndex = 10;
+            this.l.Text = "0.8";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "L:";
+            // 
+            // w
+            // 
+            this.w.Location = new System.Drawing.Point(111, 281);
+            this.w.Name = "w";
+            this.w.Size = new System.Drawing.Size(88, 31);
+            this.w.TabIndex = 12;
+            this.w.Text = "0.8";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(61, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "W:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1782, 857);
+            this.Controls.Add(this.w);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.l);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.a);
+            this.Controls.Add(this.aeqwe);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.n);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox p1;
+        private System.Windows.Forms.TextBox p2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox n;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.TextBox a;
+        private System.Windows.Forms.Label aeqwe;
+        private System.Windows.Forms.TextBox l;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox w;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ mod/lab3/Lab3/Lab3/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double[] CalcValues(double p1, double p2, int n)
+        {
+            int input = 0,
+                output = 0,
+                l = 0,
+                w = 0;
+            string state = "2000";
+            Dictionary<string, int> states = new Dictionary<string, int>();
+            void calcState(double randP1, double randP2) {
+               if (state[2] == '1' && randP1 > p1) {
+                    state = getNewState(state, 0, 2);
+                    output++;
+               }
+               if (state[3] == '1' && randP2 > p2) {
+                    state = getNewState(state, 0, 3);
+                    output++;
+               }
+               if (state[1] == '1' && (state[2] == '0' || state[3] == '0'))
+                {
+                    if (state[2] =='0')
+                    {
+                        int j = Convert.ToInt32(state[1]);
+                        if (j - 1 >= 0)
+                        {
+                            state = getNewState(state, j - 1, 1);
+                            state = getNewState(state, 1, 2);
+                        }
+                    } else if (state[3] == '0')
+                    {
+                        int j = Convert.ToInt32(state[1]);
+                        if (j - 1 >= 0)
+                        {
+                            state = getNewState(state, j - 1, 1);
+                            state = getNewState(state, 1, 3);
+                        }
+                    }
+                }
+            };
+
+            states.Add(state, 0);
+
+            for (int i = 0; i < n; i++)
+            {
+                Double randP1 = getRandom(),
+                       randP2 = getRandom();
+                switch(state[0])
+                {
+                    case '1':
+                        calcState(randP1, randP2);
+                        if (state.Equals("1111"))
+                        {
+
+                        } else
+                        {
+                            int j = Convert.ToInt32(state[1]);
+                            if (j + 1 < 2)
+                            {
+                                j += 1;
+                                state = getNewState(state, j, 1);
+                                if (j - 1 >= 0 && state[2] == '0')
+                                {
+                                    j -= 1;
+                                    state = getNewState(state, j, 1);
+                                    state = getNewState(state, 1, 2);
+                                }
+                                if (j - 1 >= 0 && state[3] == '0')
+                                {
+                                    j -= 1;
+                                    state = getNewState(state, j, 1);
+                                    state = getNewState(state, 1, 3);
+                                }
+                            }
+                            input++;
+                        }
+                        state = getNewState(state, 2, 0);
+                        break;
+                    case '2':
+                        calcState(randP1, randP2);
+                        state = getNewState(state, 1, 0);
+                        break;
+                    default:
+                        break;
+                }
+                int jTemp = Convert.ToInt32(state[1]);
+                w += jTemp;
+                updateState(states, state);
+                l += Convert.ToInt32(state[1]) > 0 ? 1 : 0;
+            }
+
+            return new double[]{ output / n, l / n, w / output };
+        }
+        private string getNewState(string state, int newValue, int valueIndex)
+        {
+            var arrString = state.ToCharArray();
+            arrString[valueIndex] = newValue.ToString().ToCharArray()[0];
+            return new string(arrString);
+        }
+        private void updateState(Dictionary<string, int> states, string key)
+        {
+            int value;
+            if (states.TryGetValue(key, out value) == true)
+            {
+                states.Remove(key);
+                states.Add(key, value + 1);
+            } else {
+                states.Add(key, 1);
+            }
+        }
+        private double getRandom()
+        {
+            Random rand = new Random();
+            return rand.NextDouble();
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            var res = CalcValues(Double.Parse(p1.Text), Double.Parse(p2.Text), Int32.Parse(n.Text));
+            a.Text = res[0].ToString();
+            l.Text = res[1].ToString();
+            w.Text = res[3].ToString();
+        }
+    }
+}
