4d9bdd55f6f267128762b0d15d4d0c725fb9dd78
AndreiKozyakov <andreikozyakov@geotab.com>
2017-12-28 03:45:52 -0500

    Update lab3 mod

--- mod/lab3/Lab3/Lab3/Form1.Designer.cs
+++ mod/lab3/Lab3/Lab3/Form1.Designer.cs
@@ -41,64 +41,73 @@
             this.label5 = new System.Windows.Forms.Label();
             this.w = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.pOtk = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 50);
+            this.label1.Location = new System.Drawing.Point(30, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 25);
+            this.label1.Size = new System.Drawing.Size(23, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "P1:";
             // 
             // p1
             // 
-            this.p1.Location = new System.Drawing.Point(111, 47);
+            this.p1.Location = new System.Drawing.Point(56, 24);
+            this.p1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.p1.Name = "p1";
-            this.p1.Size = new System.Drawing.Size(88, 31);
+            this.p1.Size = new System.Drawing.Size(46, 20);
             this.p1.TabIndex = 1;
             this.p1.Text = "0.8";
             // 
             // p2
             // 
-            this.p2.Location = new System.Drawing.Point(292, 47);
+            this.p2.Location = new System.Drawing.Point(146, 24);
+            this.p2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.p2.Name = "p2";
-            this.p2.Size = new System.Drawing.Size(88, 31);
+            this.p2.Size = new System.Drawing.Size(46, 20);
             this.p2.TabIndex = 3;
-            this.p2.Text = "0.5";
+            this.p2.Text = "0.6";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(242, 50);
+            this.label2.Location = new System.Drawing.Point(121, 26);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 25);
+            this.label2.Size = new System.Drawing.Size(23, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "P2:";
             // 
             // n
             // 
-            this.n.Location = new System.Drawing.Point(504, 47);
+            this.n.Location = new System.Drawing.Point(252, 24);
+            this.n.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.n.Name = "n";
-            this.n.Size = new System.Drawing.Size(88, 31);
+            this.n.Size = new System.Drawing.Size(46, 20);
             this.n.TabIndex = 5;
             this.n.Text = "100000";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(452, 50);
+            this.label3.Location = new System.Drawing.Point(226, 26);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 25);
+            this.label3.Size = new System.Drawing.Size(18, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "N:";
             // 
             // run
             // 
-            this.run.Location = new System.Drawing.Point(649, 47);
+            this.run.Location = new System.Drawing.Point(210, 63);
+            this.run.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.run.Name = "run";
-            this.run.Size = new System.Drawing.Size(139, 51);
+            this.run.Size = new System.Drawing.Size(88, 41);
             this.run.TabIndex = 6;
             this.run.Text = "Run";
             this.run.UseVisualStyleBackColor = true;
@@ -106,60 +115,85 @@
             // 
             // a
             // 
-            this.a.Location = new System.Drawing.Point(111, 161);
+            this.a.Location = new System.Drawing.Point(56, 84);
+            this.a.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.a.Name = "a";
-            this.a.Size = new System.Drawing.Size(88, 31);
+            this.a.Size = new System.Drawing.Size(75, 20);
             this.a.TabIndex = 8;
-            this.a.Text = "0.8";
             // 
             // aeqwe
             // 
             this.aeqwe.AutoSize = true;
-            this.aeqwe.Location = new System.Drawing.Point(61, 164);
+            this.aeqwe.Location = new System.Drawing.Point(30, 85);
+            this.aeqwe.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.aeqwe.Name = "aeqwe";
-            this.aeqwe.Size = new System.Drawing.Size(32, 25);
+            this.aeqwe.Size = new System.Drawing.Size(17, 13);
             this.aeqwe.TabIndex = 7;
             this.aeqwe.Text = "A:";
             // 
             // l
             // 
-            this.l.Location = new System.Drawing.Point(111, 219);
+            this.l.Location = new System.Drawing.Point(56, 114);
+            this.l.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.l.Name = "l";
-            this.l.Size = new System.Drawing.Size(88, 31);
+            this.l.Size = new System.Drawing.Size(75, 20);
             this.l.TabIndex = 10;
-            this.l.Text = "0.8";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(61, 222);
+            this.label5.Location = new System.Drawing.Point(30, 115);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(30, 25);
+            this.label5.Size = new System.Drawing.Size(16, 13);
             this.label5.TabIndex = 9;
             this.label5.Text = "L:";
             // 
             // w
             // 
-            this.w.Location = new System.Drawing.Point(111, 281);
+            this.w.Location = new System.Drawing.Point(56, 146);
+            this.w.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.w.Name = "w";
-            this.w.Size = new System.Drawing.Size(88, 31);
+            this.w.Size = new System.Drawing.Size(75, 20);
             this.w.TabIndex = 12;
-            this.w.Text = "0.8";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(61, 284);
+            this.label6.Location = new System.Drawing.Point(30, 148);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 25);
+            this.label6.Size = new System.Drawing.Size(21, 13);
             this.label6.TabIndex = 11;
             this.label6.Text = "W:";
             // 
+            // pOtk
+            // 
+            this.pOtk.Location = new System.Drawing.Point(69, 175);
+            this.pOtk.Margin = new System.Windows.Forms.Padding(2);
+            this.pOtk.Name = "pOtk";
+            this.pOtk.Size = new System.Drawing.Size(75, 20);
+            this.pOtk.TabIndex = 14;
+            this.pOtk.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 178);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ротк:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1782, 857);
+            this.ClientSize = new System.Drawing.Size(891, 446);
+            this.Controls.Add(this.pOtk);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.w);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.l);
@@ -173,6 +207,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.p1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -195,6 +230,8 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox w;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox pOtk;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- mod/lab3/Lab3/Lab3/Form1.cs
+++ mod/lab3/Lab3/Lab3/Form1.cs
@@ -16,13 +16,14 @@ namespace Lab3
         {
             InitializeComponent();
         }
-
         private double[] CalcValues(double p1, double p2, int n)
         {
             int input = 0,
                 output = 0,
+                otk = 0,
                 l = 0,
                 w = 0;
+            Random rand1 = new Random(new Random().Next(20400434,70234034));
             string state = "2000";
             Dictionary<string, int> states = new Dictionary<string, int>();
             void calcState(double randP1, double randP2) {
@@ -38,7 +39,7 @@ namespace Lab3
                 {
                     if (state[2] =='0')
                     {
-                        int j = Convert.ToInt32(state[1]);
+                        int j = GetInt(state[1]);
                         if (j - 1 >= 0)
                         {
                             state = getNewState(state, j - 1, 1);
@@ -46,7 +47,7 @@ namespace Lab3
                         }
                     } else if (state[3] == '0')
                     {
-                        int j = Convert.ToInt32(state[1]);
+                        int j = GetInt(state[1]);
                         if (j - 1 >= 0)
                         {
                             state = getNewState(state, j - 1, 1);
@@ -60,18 +61,18 @@ namespace Lab3
 
             for (int i = 0; i < n; i++)
             {
-                Double randP1 = getRandom(),
-                       randP2 = getRandom();
+                Double randP1 = rand1.NextDouble(),
+                       randP2 = rand1.NextDouble();
                 switch(state[0])
                 {
                     case '1':
                         calcState(randP1, randP2);
                         if (state.Equals("1111"))
                         {
-
+                            otk += 1;
                         } else
                         {
-                            int j = Convert.ToInt32(state[1]);
+                            int j = GetInt(state[1]);
                             if (j + 1 < 2)
                             {
                                 j += 1;
@@ -100,19 +101,23 @@ namespace Lab3
                     default:
                         break;
                 }
-                int jTemp = Convert.ToInt32(state[1]);
+                int jTemp = GetInt(state[1]);
                 w += jTemp;
                 updateState(states, state);
-                l += Convert.ToInt32(state[1]) > 0 ? 1 : 0;
+                l += GetInt(state[1]) > 0 ? 1 : 0;
             }
 
-            return new double[]{ output / n, l / n, w / output };
+            return new double[]{ (double)(output) / n, (double)(l) / n, (double)(w) / output, (double)otk / n };
         }
         private string getNewState(string state, int newValue, int valueIndex)
         {
             var arrString = state.ToCharArray();
             arrString[valueIndex] = newValue.ToString().ToCharArray()[0];
             return new string(arrString);
+        }
+        private int GetInt(char value)
+        {
+            return (int)Char.GetNumericValue(value);
         }
         private void updateState(Dictionary<string, int> states, string key)
         {
@@ -125,18 +130,24 @@ namespace Lab3
                 states.Add(key, 1);
             }
         }
-        private double getRandom()
-        {
-            Random rand = new Random();
-            return rand.NextDouble();
-        }
 
         private void run_Click(object sender, EventArgs e)
         {
             var res = CalcValues(Double.Parse(p1.Text), Double.Parse(p2.Text), Int32.Parse(n.Text));
             a.Text = res[0].ToString();
             l.Text = res[1].ToString();
-            w.Text = res[3].ToString();
+            w.Text = res[2].ToString();
+            pOtk.Text = res[3].ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
