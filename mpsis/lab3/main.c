3c199231708fe3d87b158254ba5dab191e699340
kosiak_man <andrushkin2@live.ru>
2018-03-13 22:55:29 +0300

    Add timer interrupts events

--- mpsis/lab3/main.c
+++ mpsis/lab3/main.c
@@ -21,8 +21,9 @@ volatile unsigned int enabled = 0;
 volatile unsigned int blinking = 0;
 volatile unsigned int delay = 0;
 
-static volatile int g_phase = 0;
-static volatile unsigned int g_period = 32768;
+static const unsigned int STEP = 4096;
+static volatile int phaseValue = 0;
+static volatile unsigned int periodValue = 32768;
 
 /*
  =========================================
@@ -76,6 +77,14 @@ static void TurnLedsOff()
     P8OUT &= ~(BIT2);
 }
 
+// Init timers state
+static void InitTimers()
+{
+    TA0CCTL0 = CCIE;    // enable interrupt on compare
+    TA0CCR0 = periodValue; // set timer period
+    TA0CTL = TASSEL_2 + MC_1 + TACLR + ID__8;
+}
+
 // Enable interrupts for buttons ports
 static void EnableInterrupts()
 {
@@ -84,32 +93,6 @@ static void EnableInterrupts()
 
     P2IES &= ~(BIT2); // Enable interrupt Low-High
     P2IE |= BIT2;     // Enable interrupts for the second port
-
-    __bis_SR_register(GIE); // Enable processing of interrupts
-}
-
-static void processBlinking()
-{
-    if (--delay == 0)
-    {
-        if (blinking > 0 && enabled > 0)
-        {
-            // blinking is ON -> switch leds to opposite state
-            TurnLedsToOppositeState();
-        }
-        else if (blinking == 0 && enabled > 0)
-        {
-            //blinking is OFF -> leds is ON
-            TurnLedsOn();
-        }
-        else
-        {
-            //Programm is OFF -> lights is OFF
-            TurnLedsOff();
-        }
-
-        delay = 6000;
-    }
 }
 
 int main(void)
@@ -119,18 +102,32 @@ int main(void)
 
     // init states of controls
     InitLeds();
-    InitButtons();
-
     // turn leds off
-    TurnLedsOff();
+    TurnLedsOn();
 
+    InitButtons();
     // enable interrupts
     EnableInterrupts();
 
-    while (1)
-    {
-        // process the programm state
-        processBlinking();
+    // init timers
+    InitTimers();
+
+    __bis_SR_register(GIE); // Enable processing of interrupts
+    __no_operation();
+}
+
+// timer interrupt
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void Timer0_A0()
+{
+    TurnLedsToOppositeState();
+
+    if (phaseValue) {
+        phaseValue = 0;
+        TA0CCR0 = 65535 - periodValue;
+    } else {
+        phaseValue = 1;
+        TA0CCR0 = periodValue;
     }
 }
 
@@ -138,22 +135,14 @@ int main(void)
 #pragma vector = PORT1_VECTOR
 __interrupt void PORT1_ISR(void)
 {
-    for (int i = 0; i < 30; i++)
-        ;
+    int i;
+    // waiting for noise canceling
+    for (i = 0; i < 50; i++);
 
     // load button S1 state
-    if (P1IN & BIT7)
-    {
-        if (enabled == 0)
-        {
-            // If programm is OFF -> set programm to ON state
-            enabled = 1;
-        }
-        else if (enabled == 1)
-        {
-            // If programm is ON -> set programm to OFF state, blinking is OFF
-            enabled = 0;
-            blinking = 0;
+    if (P1IN & BIT7) {
+        if (periodValue < 65535 - STEP) {
+            periodValue += STEP
         }
     }
 
@@ -165,21 +154,14 @@ __interrupt void PORT1_ISR(void)
 #pragma vector = PORT2_VECTOR
 __interrupt void PORT2_ISR(void)
 {
-    for (int i = 0; i < 30; i++)
-        ;
+    int i;
+    // waiting for noise canceling
+    for (int i = 0; i < 50; i++);
 
     // load button S2 state
-    if (P1IN & BIT7)
-    {
-        if (blinking == 0 && enabled == 1)
-        {
-            // If programm is ON and blinking is OFF -> set blinking to ON state
-            blinking = 1;
-        }
-        else if (blinking == 1 && enabled == 1)
-        {
-            // If programm is ON and blinking is ON -> set blinking to OFF state
-            blinking = 0;
+    if (P1IN & BIT7) {
+        if (periodValue > STEP) {
+            periodValue -= STEP;
         }
     }
 
